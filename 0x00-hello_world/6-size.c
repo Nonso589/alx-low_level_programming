b840ba40dfa0891c512d556a0d360eecfa058fad
Chinonso <Nonsodavid433@gmail.com>
2023-02-16 14:27:00 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+int main(void)
+{      char a;
+	int b;
+	long int c;
+	float f;
+printf("size of a char: %lu byte(s)\n", (unsignedlong)sizeof(a));
+printf("size of an int: %lu bytes(s)
