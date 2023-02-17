64b65c94e9c07302efe6f032bdda69ef1ec91004
Chinonso <Nonsodavid433@gmail.com>
2023-02-17 13:05:58 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - write a program to print lower case Return: 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+
+	{
+		if (la != 'e' && la != 'a')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
