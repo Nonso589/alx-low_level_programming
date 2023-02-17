6cb3577bc885cb4cbcedce9e8a2aec0a1067fe12
Chinonso <Nonsodavid433@gmail.com>
2023-02-17 13:32:40 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that prints all single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; a < 10; num++)
+		printf("%d", num);
+
+	printf("%d");
+
+	return (0);
+}
