7a951fc0acfdc8d83bdcce734bfbf1c95f4c1231
Chinonso <Nonsodavid433@gmail.com>
2023-05-22 07:29:55 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,9 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-/*all functions prototypes*/
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -26,5 +24,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
 #endif /* MAIN_H */
